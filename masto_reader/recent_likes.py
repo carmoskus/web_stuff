fa66be74f8456be659d0d40a1542a46ff630921b
Chris Armoskus <carmoskus@gmail.com>
2024-11-24 10:03:19 -0800

    Removed testing code; working well

--- masto_reader/recent_likes.py
+++ masto_reader/recent_likes.py
@@ -1,25 +1,17 @@
 #
 
 import requests, re
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
 # url = "https://mastodon.online/api/v1/accounts/111183843398906311" # account
 # url = "https://mastodon.online/api/v1/accounts/111183843398906311/following" # following
 # url = "https://mastodon.online/api/v1/accounts/111183843398906311/lists" # lists (needs login)
-url = "https://mastodon.online/api/v1/accounts/111183843398906311/statuses?limit=10" # statuses
+# url = "https://mastodon.online/api/v1/accounts/111183843398906311/statuses?limit=10" # statuses
 
-r = requests.get(url)
-r
+# j = r.json()
 
-r.headers['Content-Type']
-
-# print(r.text)
-j = r.json()
-j[0].keys()
-
-for k in j[0].keys():
-    print(k, type(j[0][k]))
+# j[0].keys()
 # id <class 'str'>
 # created_at <class 'str'>
 # in_reply_to_id <class 'NoneType'>
@@ -45,29 +37,30 @@ for k in j[0].keys():
 # card <class 'NoneType'>
 # poll <class 'NoneType'>
 
-j[0]['url']
-j[0]['reblog']['url']
-
-j[0]['created_at']
-j[0]['reblog']['created_at']
-
-j[0]['content']
-j[0]['reblog']['content']
-
-html_txt = j[19]['reblog']['content']
-soup = BeautifulSoup(html_txt, 'html.parser')
-
-soup
-
-boost_list = [x for x in j if x['reblog'] is not None]
-for x in boost_list:
-    print(datetime.fromisoformat(x['created_at']).date())
-
-start_today = datetime.now().date()
-one_day_ago = start_today - timedelta(days=1)
-
-[x for x in [datetime.fromisoformat(x['created_at']).date() for x in boost_list] 
- if x < start_today and x >= one_day_ago]
+# j[0]['reblog'].keys()
+# id <class 'str'>
+# created_at <class 'str'>
+# in_reply_to_id <class 'NoneType'>
+# in_reply_to_account_id <class 'NoneType'>
+# sensitive <class 'bool'>
+# spoiler_text <class 'str'>
+# visibility <class 'str'>
+# language <class 'str'>
+# uri <class 'str'>
+# url <class 'str'>
+# replies_count <class 'int'>
+# reblogs_count <class 'int'>
+# favourites_count <class 'int'>
+# edited_at <class 'NoneType'>
+# content <class 'str'>
+# reblog <class 'NoneType'>
+# account <class 'dict'>
+# media_attachments <class 'list'>
+# mentions <class 'list'>
+# tags <class 'list'>
+# emojis <class 'list'>
+# card <class 'dict'>
+# poll <class 'NoneType'>
 
 """
 Function to parse a BeautifulSoup object into needed info
@@ -99,6 +92,7 @@ def parse_soup(soup):
         for a in main_links: 
             a.decompose()
         parsed_txt = " ".join(x.strip() for x in soup.get_text("\n").split("\n") if x.strip() != "")
+        # TODO fix a similar issue with @ as we deal with here for #
         parsed_txt = parsed_txt.replace(" # ", " ")
         return len(main_links), a_hrefs, parsed_txt
     parsed_txt = " ".join(x.strip() for x in soup.get_text("\n").split("\n") if x.strip() != "")
@@ -133,28 +127,28 @@ def get_daily_boosts(acc_id: str, my_date, max_id: str = ''):
         nm_txt = f"[{nm}]({e_url})"
         soup = BeautifulSoup(entry['reblog']['content'], 'html.parser')
         parsed = parse_soup(soup)
-        results.append((nm_txt, ) + parsed)
-        # if parsed[0] == 0:
-        #     print(nm_txt)
-        #     print('>', parsed[1])
-        # elif parsed[0] == 1:
-        #     print(nm_txt, '-', "<"+parsed[1]+">")
-        #     print('>', parsed[2])
-        # else:
-        #     print(nm_txt, '???')
-        # print()
+        results.append((nm_txt, ) + parsed)        
     # Check if the oldest entry we pulled is before one_day_before
     if dates[-1] < one_day_before:
         # Then we are finished
         return results
+    
+    # TODO compare to using the LINK header info
     new_max_id = json[-1]['id']
     return results + get_daily_boosts(acc_id, my_date, new_max_id)
 
-resX = get_daily_boosts('111183843398906311', datetime.now().date() - timedelta(days=-1))
-res_list = [get_daily_boosts('111183843398906311', datetime.now().date() - timedelta(days=i))
-            for i in range(5)]
 def md_escape_text(parsed_string: str):
-    return parsed_string.replace('<', '\<').replace('#', '\#')
+    return parsed_string.replace(
+        '<', '\<').replace(
+        '#', '\#').replace(
+        '[', '\[').replace(
+        '(', '\(').replace(
+        '*', '\*')
+
+# --- MAIN
+res_list = [get_daily_boosts('111183843398906311', datetime.now().date() - timedelta(days=i))
+            for i in range(2)]
+
 for i, res in enumerate(res_list):
     print(i, "---")
     for parsed in res[::-1]:
@@ -171,82 +165,3 @@ for i, res in enumerate(res_list):
                 print('<'+a_href+'>')
             print('>', md_escape_text(parsed[3]))
         print()
-
-
-
-soup_list = [BeautifulSoup(x['reblog']['content'], 'html.parser') for x in boost_list]
-for i, soup in enumerate(soup_list):
-    main_links = []
-    for a in soup.find_all('a'):
-        a_class = a.get('class')
-        # TODO: add check that href actually exists
-        if a_class is not None and ('mention' in a_class or 'hashtag' in a_class):
-            # a.decompose()
-            continue
-        main_links.append(a)
-    print(i, len(main_links))
-    if len(main_links) == 1:
-        a = main_links[0]
-        a_href = a['href']
-        a_text = a.get_text()
-        a.decompose()
-        parsed_txt = " ".join(x.strip() for x in soup.get_text("\n").split("\n") if x.strip() != "")
-        parsed_txt = parsed_txt.replace(" # ", " #")
-        print(a_href)
-        print(parsed_txt)
-    elif len(main_links) > 1:
-        # Might break tie if one of the links has an old date
-        print('ERROR:', main_links)
-    else:
-        parsed_txt = " ".join(x.strip() for x in soup.get_text("\n").split("\n") if x.strip() != "")
-        parsed_txt = parsed_txt.replace(" # ", " #")
-        print(parsed_txt)
-    print()
-
-
-j[0]['reblog']
-j[0]['reblog']['reblog']
-
-j[0]['reblog'].keys()
-for k, v in j[0]['reblog'].items():
-    print(k, type(v))
-# id <class 'str'>
-# created_at <class 'str'>
-# in_reply_to_id <class 'NoneType'>
-# in_reply_to_account_id <class 'NoneType'>
-# sensitive <class 'bool'>
-# spoiler_text <class 'str'>
-# visibility <class 'str'>
-# language <class 'str'>
-# uri <class 'str'>
-# url <class 'str'>
-# replies_count <class 'int'>
-# reblogs_count <class 'int'>
-# favourites_count <class 'int'>
-# edited_at <class 'NoneType'>
-# content <class 'str'>
-# reblog <class 'NoneType'>
-# account <class 'dict'>
-# media_attachments <class 'list'>
-# mentions <class 'list'>
-# tags <class 'list'>
-# emojis <class 'list'>
-# card <class 'dict'>
-# poll <class 'NoneType'>
-
-
-#
-url = "https://mastodon.online/api/v1/accounts/111183843398906311/statuses" # statuses
-
-r = requests.get(url)
-r.headers['Content-Type']
-r.headers['Link']
-
-x = re.search(r'[&?]max_id=(\d+)', r.headers['Link'])
-x.group(1)
-
-url2 = "https://mastodon.online/api/v1/accounts/111183843398906311/statuses?max_id=" + x.group(1)
-
-r2 = requests.get(url2)
-r2.headers['Content-Type']
-r2.headers['Link']
