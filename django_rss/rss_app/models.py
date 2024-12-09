26e612fb2c1bf66c3af87fb6f8645df84cc6ba2f
Chris Armoskus <carmoskus@gmail.com>
2024-12-09 13:48:48 -0800

    Adding models based on what I had for RSS in sqlite

--- django_rss/rss_app/models.py
+++ django_rss/rss_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Source(models.Model):
+    url = models.URLField(max_length=200)
+
+class Fetch(models.Model):
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+
+class Item(models.Model):
+    fetch = models.ForeignKey(Fetch, on_delete=models.CASCADE)
+    url = models.URLField(max_length=200)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_time = models.DateTimeField()
+    
