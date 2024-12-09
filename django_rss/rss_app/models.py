7dbb389944387015992e48c54ff719e888788967
Chris Armoskus <carmoskus@gmail.com>
2024-12-09 15:48:53 -0800

    More models

--- django_rss/rss_app/models.py
+++ django_rss/rss_app/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Source(models.Model):
-    url = models.URLField(max_length=200)
-
-class Fetch(models.Model):
-    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    url = models.URLField(max_length=200, unique=True)
+    
+    def __str__(self):
+        return self.url
 
 class Item(models.Model):
-    fetch = models.ForeignKey(Fetch, on_delete=models.CASCADE)
-    url = models.URLField(max_length=200)
+    source = models.ForeignKey(Source, on_delete=models.SET_NULL, null=True)
+    url = models.URLField(max_length=200, unique=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     content = models.TextField()
-    pub_time = models.DateTimeField()
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.url
     
