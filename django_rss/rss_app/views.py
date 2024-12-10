cac6206178b599b58a6ffc09692fab13b93f8b0d
Chris Armoskus <carmoskus@gmail.com>
2024-12-09 17:05:59 -0800

    First template

--- django_rss/rss_app/views.py
+++ django_rss/rss_app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.utils.html import escape
+
+from .models import Source
 
 def index(request):
-    return HttpResponse("Hello world. Next stop RSS")
+    all_sources = Source.objects.all()
+    context = {"source_list": all_sources}
+    return render(request, "rss_app/index.html", context)
+    # return HttpResponse("<p>Hello world. Next stop RSS:</p><code>" + escape(str(all_sources)) + "</code>")
