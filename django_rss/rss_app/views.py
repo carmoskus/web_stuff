a180cb555f1df7974a4bfcc255ec6e50c55290b0
Chris Armoskus <carmoskus@gmail.com>
2024-12-09 17:20:28 -0800

    Added a detail view

--- django_rss/rss_app/views.py
+++ django_rss/rss_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,4 +10,8 @@ def index(request):
     all_sources = Source.objects.all()
     context = {"source_list": all_sources}
     return render(request, "rss_app/index.html", context)
-    # return HttpResponse("<p>Hello world. Next stop RSS:</p><code>" + escape(str(all_sources)) + "</code>")
+
+def source(request, source_id):
+    source = get_object_or_404(Source, pk=source_id)
+    context = {"source": source}
+    return render(request, "rss_app/source.html", context)
