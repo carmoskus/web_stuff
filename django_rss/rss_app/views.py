b0a7d751c1cd766bc0c63867dc742e92bcb7adaa
Chris Armoskus <carmoskus@gmail.com>
2024-12-12 18:28:25 -0800

    Viewing XKCD

--- django_rss/rss_app/views.py
+++ django_rss/rss_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.utils.html import escape
 
-from .models import Source
+from .models import Source, Item
 
 def index(request):
     all_sources = Source.objects.all()
@@ -13,5 +13,6 @@ def index(request):
 
 def source(request, source_id):
     source = get_object_or_404(Source, pk=source_id)
-    context = {"source": source}
+    items = Item.objects.filter(source=source)
+    context = {"source": source, "items": items}
     return render(request, "rss_app/source.html", context)
