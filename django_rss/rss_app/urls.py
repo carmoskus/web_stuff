1bb3a3bbf1b191206d67ebc5e7388575b0d3e606
Chris Armoskus <carmoskus@gmail.com>
2024-12-12 19:24:52 -0800

    Fetch form and more output

--- django_rss/rss_app/urls.py
+++ django_rss/rss_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("source/<int:source_id>/", views.source, name="source"),
+    path("source/<int:source_id>/fetch/", views.fetch, name="fetch"),
 ]
--- django_rss/rss_app/views.py
+++ django_rss/rss_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils.html import escape
+from django.urls import reverse
 
 from .models import Source, Item
+
+import feedparser, datetime, time
 
 def index(request):
     all_sources = Source.objects.all()
@@ -16,3 +19,28 @@ def source(request, source_id):
     items = Item.objects.filter(source=source)
     context = {"source": source, "items": items}
     return render(request, "rss_app/source.html", context)
+
+def fetch(request, source_id):
+    source = get_object_or_404(Source, pk=source_id)
+    feed = feedparser.parse(source.url)
+    if feed.bozo:
+        return HttpResponse(status=500)
+
+    for entry in feed.entries:
+        content = entry.summary
+        if len(entry.get("content", [])) > 0:
+            content = "\n".join(x.value for x in entry.content)
+
+        author = entry.get('author', '')
+        if author.startswith("by "):
+            author = author[:3]
+
+        obj = Item(source=source, 
+                url=entry.id, 
+                title=entry.title, 
+                author=author, 
+                content=content,
+                pub_date=datetime.datetime.fromtimestamp(time.mktime(entry.published_parsed)))
+        obj.save()
+
+    return HttpResponseRedirect(reverse("source", args=(source.id,)))
